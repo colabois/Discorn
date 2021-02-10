4259fdc3e00b94e57e3ae12c9a7b30e5e6f06dc6
SuwakoMmh <FUSRODAH64d>
2021-02-10 12:50:18 +0100

    Added MerkleTree + Mining

--- src/guild/blockchain.py
+++ src/guild/blockchain.py
@@ -5,34 +5,72 @@ import hashlib
 from log import Logger
 import base58
 import os
+import time
+import string
+from merklelib import MerkleTree
 
 
-class Block:
+def fast_hash(b):
+    return pycryptonight.cn_fast_hash(b).hex()
+
+
+class Block(Logger):
     """Block and header definition."""
 
-    NONCE_SIZE = 64
+    NONCE_SIZE = 4
 
-    def __init__(self, version=0, corners=None, previous_hash=pycryptonight.cn_slow_hash(b''),
+    def __init__(self,
+                 blockchain=None,
+                 name='block',
+                 height=0,
+                 version=0,
+                 coinbase=None,
+                 corners=None,
+                 timestamp=0,
+                 previous_hash=pycryptonight.cn_slow_hash(b''),
                  nonce=(0).to_bytes(NONCE_SIZE, 'big')):
+        super().__init__(f"{name} - {height} :")
+        self.blockchain = blockchain
         self.version = version
-        self.merkle_root = pycryptonight.cn_fast_hash(b'')
+        self.timestamp = timestamp
         self.previous_hash = previous_hash
         self.nonce = nonce
-        self.corners = []  # index 0 is coinbase.
+        self.coinbase = coinbase
+        self._corners = [] if corners is None else corners
+        self.merkle_tree = MerkleTree(self.corners, fast_hash)
         self.header = self.encode_header()
         self.hash = self.get_hash()
 
+    @property
+    def corners(self):
+        return [self.coinbase] + self._corners
+
+    def compute_tree(self, new_data=None):
+        if new_data is None:
+            self.merkle_tree = MerkleTree(self.corners, fast_hash)
+        else:
+            self.merkle_tree.extend(new_data)
+
     def encode_header(self):
         res = self.version.to_bytes(2, 'big')
+        res += self.timestamp.to_bytes(8, 'big')
         res += len(self.corners).to_bytes(3, 'big')
-        res += self.merkle_root
+        res += bytes.fromhex(self.merkle_tree.merkle_root)
         res += self.previous_hash
         res += self.nonce
         self.header = res
         return res
 
     def random_nonce(self):
-        self.nonce = os.urandom(64)
+        self.timestamp = time.time_ns()
+        self.nonce = os.urandom(self.NONCE_SIZE)
+
+    def mine(self, difficulty=4):
+        while int.from_bytes(self.get_hash(), 'big') >= (1 << (256 - difficulty)):
+            self.log(f"new hash : {self.hash.hex()}")
+            self.random_nonce()
+            self.encode_header()
+        self.log(f"Mined !! : {self.hash.hex()}")
 
     def get_hash(self):
         self.hash = pycryptonight.cn_slow_hash(self.header, 4)
@@ -55,12 +93,15 @@ class BlockChain(Logger):
         self.log(f"New head : [{len(self.blocks)}] - {block.hash.hex()}")
 
     def get_block_template(self):
-        pass
+        block = Block(self,
+                      corners=[corner for corner in self.unconfirmed_corners.items()],
+                      timestamp=time.time_ns(),
+                      previous_hash=self.block_hashes[-1])
 
 
 if __name__ == '__main__':
     chain = BlockChain()
-    genesis = Block()
+    genesis = Block(chain, name='Main')
     genesis.random_nonce()
     genesis.encode_header()
     genesis.get_hash()
