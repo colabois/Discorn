734f290f03559aa1ecc7b79b951b56b9fa68ab6d
SuwakoMmh <FUSRODAH64d>
2021-02-13 03:04:10 +0100

    CheckPoint

--- src/guild/blockchain.py
+++ src/guild/blockchain.py
@@ -1,3 +1,5 @@
+# TODO: Recode all of this once again.
+
 import pycryptonight
 import ecdsa
 from Crypto.Hash import RIPEMD160
@@ -14,6 +16,50 @@ def fast_hash(b):
     return pycryptonight.cn_fast_hash(b).hex()
 
 
+def get_hash(data, hash_func=hashlib.sha256):
+    h = hash_func()
+    h.update(data)
+    return h.digest()
+
+
+class SK:
+    def __init__(self, sk=None):
+        self._sk = sk
+        if self._sk is None:
+            self._sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256)
+        self._vk = self._sk.verifying_key
+        self.vk = VK(self._vk)
+        self.address = self.vk.address
+        self.b58 = base58.b58encode(self.address, base58.BITCOIN_ALPHABET)
+
+    def sign(self, data):
+        return Signature(self._sk.sign(pycryptonight.cn_fast_hash(data)), self.vk)
+
+
+class VK:
+    def __init__(self, vk):
+        self._vk = vk
+        self.string = self._vk.to_string()
+        self.address = get_hash(pycryptonight.cn_fast_hash(self._vk.to_string()), RIPEMD160.new)
+        self.b58 = base58.b58encode(self.address, base58.BITCOIN_ALPHABET)
+
+    def verify(self, signature, data):
+        try:
+            return self.vk.verify(signature.signature, pycryptonight.cn_fast_hash(data))
+        except ecdsa.keys.BadSignatureError:
+            return False
+
+
+class Signature(Logger):
+    def __init__(self, signature, vk):
+        self.signature = signature
+        self.vk = vk
+
+    @property
+    def raw(self):
+        return self.vk.string + self.signature
+
+
 class Corner:
     def __init__(self, flag=-1):
         self.flag = flag
@@ -28,11 +74,29 @@ class Corner:
 
 
 class Tx(Corner):
-    def __init__(self, version=0, inputs=None, outputs=None):
+    def __init__(self, version=0, inputs=None, outputs=None, signatures=None):
         self.flag = 0
         self.version = version
         self.inputs = [] if inputs is None else inputs
         self.outputs = [] if outputs is None else outputs
+        self.signatures = [] if signatures is None else signatures
+
+    @property
+    def body(self):
+        res = self.version.to_bytes(1, 'big')
+        res += len(self.inputs).to_bytes(1)
+        for (address, origin) in self.inputs:
+            res += address + origin
+        res += len(self.outputs)
+        for (address, amount) in self.outputs:
+            res += address + amount.to_bytes()
+
+    @property
+    def payload(self):
+        res = self.body
+        for signature in self.signatures:
+            res += signature.raw
+        return res
 
 
 class Authority:
@@ -48,11 +112,20 @@ class PoW(Authority):
 
 
 class Event(Corner):
-    def __init__(self, version=0, event_hash=pycryptonight.cn_fast_hash(b''), authority=None):
+    authority = b''
+
+    def __init__(self, version=0, event_hash=pycryptonight.cn_fast_hash(b'')):
         self.flag = 1
         self.version = version
         self.event_hash = event_hash
-        self.authority = PoW if authority is None else authority
+        self.authority_flag = -1
+
+    @property
+    def payload(self):
+        res = self.version.to_bytes(1, 'big')
+        res += self.event_hash
+        res += self.authority_flag(1, 'big')
+        res += self.authority
 
 
 class Block(Logger):
@@ -80,6 +153,7 @@ class Block(Logger):
         self._corners = [] if corners is None else corners
         self.merkle_tree = MerkleTree(self.corners, fast_hash)
         self.hash = self.get_hash()
+        self.difficulty = 4
 
     @property
     def corners(self):
@@ -105,7 +179,8 @@ class Block(Logger):
         self.timestamp = time.time_ns()
         self.nonce = os.urandom(self.NONCE_SIZE)
 
-    def mine(self, difficulty=4):
+    def mine(self, difficulty=None):
+        difficulty = self.difficulty if difficulty is None else difficulty
         while int.from_bytes(self.get_hash(), 'big') >= (1 << (256 - difficulty)):
             self.log(f"new hash : {self.hash.hex()}")
             self.random_nonce()
@@ -136,6 +211,14 @@ class BlockChain(Logger):
                       corners=[corner for corner in self.unconfirmed_corners.items()],
                       timestamp=time.time_ns(),
                       previous_hash=self.block_hashes[-1])
+
+    def check_tx(self, tx):  # TODO: Check that the fee is positive ; Inputs exist and are not spent ;
+        pass
+
+    def check_block(self, block):  # TODO: * Check output value for Coinbase and check every other Corner
+        res = block.previous_hash in self.blocks
+        res = res and int.from_bytes(block.get_hash(), 'big') >= (1 << (256 - block.difficulty))
+        return res
 
 
 if __name__ == '__main__':
